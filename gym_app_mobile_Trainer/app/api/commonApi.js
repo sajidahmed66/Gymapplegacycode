098f89d7e093d2ccc7b695ee4bae30fd77aee1b1
sajidahmed66 <sajidahmedsiddiqui66@gmail.com>
2022-10-07 12:45:15 +0600

    legacy code shifted from bitbucket to github for showcas

---
+++ gym_app_mobile_Trainer/app/api/commonApi.js
@@ -0,0 +1,11 @@
+const commonApi = {
+    // api: "http://localhost:/5000/",
+    // api: "http://192.168.1.103:5000/",
+    // api: "http://192.168.50.180:5000/",
+    //api: "http://192.168.43.109:5000/",
+    api: "https://gym--app.herokuapp.com/",
+    // api: "http://192.168.0.114:5000/",
+
+}
+
+export default commonApi;
